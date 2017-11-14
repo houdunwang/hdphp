c183a308376df8490c55795f971f900e7bc88702
houdunwang <2300071698@qq.com>
2017-11-14 20:24:47 +0800

    初始项目

---
+++ app/home/controller/Entry.php
@@ -0,0 +1,21 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Software: [HDPHP framework]
+ * |      Site: www.hdphp.com  www.hdcms.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+
+namespace app\home\controller;
+
+use houdunwang\route\Controller;
+
+class Entry extends Controller
+{
+    public function index()
+    {
+        return View::with('framework', 'HDPHP')->make();
+    }
+}
